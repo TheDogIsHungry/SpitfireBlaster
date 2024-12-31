0c246e5406afc9953f25c8f74ebed9dc87e3c7df
TheDogIsHungry <81935080+TheDogIsHungry@users.noreply.github.com>
2024-12-30 20:50:35 -0500

    Added more detailed options for comp and mode
    Replaced single char array {'N', 'Y', 'S' , 'B', 'A', 'b'} with string {"Off", "On", "Semi", "Bst", "Auto", "Bin"};

--- Display.h
+++ Display.h
@@ -42,10 +42,10 @@ hover hoverOver[13] {
 {"Mode:", 75, 23},
 {"BSize:", 75, 31},
 {"Save", 105, 54},
-{"Forward", 25, 15},
-{"Middle", 25, 23},
-{"Rear", 25, 31},
 {"Back", 52, 54},
+{"Rear", 25, 31},
+{"Middle", 25, 23},
+{"Forward", 25, 15},
 
 };
 
@@ -91,7 +91,7 @@ void display_init() {         // Initializes screen, displays splash.
   Display.drawBitmap(0, 0, splash, 128, 64, 1);
 
   Display.setCursor(5,37);  //splash screen text
-  Display.println("0.12.1"); // Revision #
+  Display.println("0.9.0"); // Revision #
   Display.setCursor(5, 47);
   Display.println("WAHOO");
   Display.setCursor(5, 55);
@@ -105,15 +105,14 @@ void mainScreen() {
 
   Display.clearDisplay();
   Display.setTextSize(1);
-  Display.setTextColor(WHITE); 
+  Display.setTextColor(WHITE); // White. 
 
-  Display.setCursor(30, 0);
+  Display.setCursor(42, 0);
   Display.print("Spitfire");
-  Display.drawLine(30, 8, 76, 8, 1);
   Display.setCursor(0, 55);
-  Display.print("Beta0.12.1");
-  Display.setCursor(89, 0); 
-  Display.print(voltageRead()); 
+  Display.print("Beta0.9.0");
+  Display.setCursor(89, 0); //just the V for the voltage
+  Display.print(voltageRead()); //BETA add the actual values to print
   Display.print("V"); 
   Display.setCursor(111 - ((menuState.length() - 3) * 6), 55);  // Keep "Forward", "Middle", "Rear", as far right as possible. 
   Display.print(menuState); 
@@ -155,7 +154,6 @@ Display.clearDisplay();
   Display.setTextColor(WHITE); //white 
   Display.setCursor(40, 0);           //  Static labels.
   Display.print("Settings");
-  Display.drawLine(40, 8, 86, 8, 1);
   Display.setCursor(0, 15);
   Display.print("DPS:   ");
   Display.println(dpsSetting);
@@ -218,15 +216,12 @@ if(menuState == "Save") {     // Save menu. Allows to save to specific switch po
   Display.setTextColor(1); 
   Display.setCursor(40, 0);
   Display.print("Save to:");
-  Display.drawLine(40, 8, 81, 8, 1);
   Display.setCursor(25, 15);
-  Display.print(hoverOver[9].hoverLabel);
-  Display.setCursor(25, 23);
-  Display.print(hoverOver[10].hoverLabel);
-  Display.setCursor(25, 31);
-  Display.print(hoverOver[11].hoverLabel);
+  Display.println(hoverOver[12].hoverLabel);
+  Display.println(hoverOver[11].hoverLabel);
+  Display.println(hoverOver[10].hoverLabel);
   Display.setCursor(52, 54);
-  Display.print(hoverOver[12].hoverLabel);
+  Display.print(hoverOver[9].hoverLabel);
 
   Display.fillRect(81, 15, 9, 8, 1);
   Display.drawRect(89, 15, 9, 8, 1);
@@ -323,7 +318,7 @@ menuState = hoverOver[counterCopy].hoverLabel;                              // C
   case 8: 
      lowerBound = 9; 
      upperBound = 12;
-     counter = 9; 
+     counter = 12; 
      break;
   }
     return; 
@@ -376,12 +371,12 @@ void settingsMenu() {          // Master settings menu master function, calls pr
 		  if (DTCHECK != currentStateCLK) {
         if(counter < upperBound) {
 			  (menuState == "Hang:") ? counter += 100 : counter++;     // If on hang, increment/decrement by 100, otherwise, increment by 1. 
-        (menuState == "Settings" && modeSetting != 3 && counter == 7) ? counter = 8 : counter += 0;
+        (menuState == "Settings" && modeSetting != 3 && counter == 6) ? counter = 8 : counter += 0;
         }
 		  } else {
         if(counter > lowerBound) {
 			  (menuState == "Hang:") ? counter -= 100 : counter--; 
-        (menuState == "Settings" && modeSetting != 3 && counter == 7) ? counter = 6 : counter -= 0; 
+        (menuState == "Settings" && modeSetting != 3 && counter == 8) ? counter = 6 : counter -= 0; 
        }
 		  }  
       settingScreen(counter);
@@ -399,7 +394,7 @@ void settingsMenu() {          // Master settings menu master function, calls pr
       menuState = "Main Menu"; 
       break; 
     }
-    if (millis() - lastButtonPress > 50 && menuState == "Save" && counter != 12) {     // If save button confirmed (Pressed on anything other than '9' (back button), break from settings menu, return to void loop()
+    if (millis() - lastButtonPress > 50 && menuState == "Save" && counter != 9) {     // If save button confirmed (Pressed on anything other than '9' (back button), break from settings menu, return to void loop()
       waitHigh();
       rebootingScreen();
       break;                                                                        
