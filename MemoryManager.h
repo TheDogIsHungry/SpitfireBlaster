b82d49cb3d8f113f4cbcfa3de6a2940128fafaee
Eugenio Sotelo <69026824+DriftLoki@users.noreply.github.com>
2025-08-16 01:09:48 -0400

    Update MemoryManager.h
    comp to idle change

--- MemoryManager.h
+++ MemoryManager.h
@@ -21,9 +21,9 @@
 #define hang_mid 11
 #define hang_rear 12
 
-#define comp_for 13
-#define comp_mid 14
-#define comp_rear 15
+#define idle_for 13
+#define idle_mid 14
+#define idle_rear 15
 
 #define mode_for 16
 #define mode_mid 17
@@ -40,13 +40,13 @@ int dpsSetting;              // Rate of fire in darts per second.
 int motorspeedSetting;           // Speed of motor, 1000 - 2000.
 int brakeSetting;             // Brake amount, used as # with values 1 through 3, the higher the number the faster the motors will break.
 int hangtimeSetting;             // Time before motor revs down after trigger release, in ms.
-int compSetting;             // Tournament mode On or Off
+int idleSetting;             // Tournament mode On or Off
 int modeSetting;             // Fire mode, 4 options: Single (Semi), Burst (Bst), Auto (Auto), Binary (Bin).
 int burstSetting;            // Contains burst amount, 2 - 5 darts per trigger pull.
 String menuState = "Main Menu";  // Contains menustate at any given point, for settings menu and main menu.
 String profileSwitch = "";       // String to display current switch position, forward, middle, rear.
 
-int* modifierArray[7] = { &dpsSetting, &motorspeedSetting, &brakeSetting, &hangtimeSetting, &compSetting, &modeSetting, &burstSetting };  // Array of pointers to modify values in the settings menu.
+int* modifierArray[7] = { &dpsSetting, &motorspeedSetting, &brakeSetting, &hangtimeSetting, &idleSetting, &modeSetting, &burstSetting };  // Array of pointers to modify values in the settings menu.
 
 // FUNCTIONS =========================================
 
@@ -58,7 +58,7 @@ void loadvalues(uint8_t loadPosition) {
     motorspeedSetting = EEPROM.read(mspeed_for);   // Prefer to use math to create equivalent value, rather than using 2 bytes for values above 255.
     brakeSetting = EEPROM.read(brake_for);
     hangtimeSetting = EEPROM.read(hang_for) * 100;  
-    compSetting = EEPROM.read(comp_for); 
+    idleSetting = EEPROM.read(idle_for); 
     modeSetting = EEPROM.read(mode_for); 
     burstSetting = EEPROM.read(burst_for); 
     profileSwitch = "Front"; 
@@ -68,7 +68,7 @@ void loadvalues(uint8_t loadPosition) {
     motorspeedSetting = EEPROM.read(mspeed_mid);   
     brakeSetting = EEPROM.read(brake_mid);
     hangtimeSetting = EEPROM.read(hang_mid) * 100; 
-    compSetting = EEPROM.read(comp_mid); 
+    idleSetting = EEPROM.read(idle_mid); 
     modeSetting = EEPROM.read(mode_mid); 
     burstSetting = EEPROM.read(burst_mid); 
     profileSwitch = "Middle"; 
@@ -78,7 +78,7 @@ void loadvalues(uint8_t loadPosition) {
     motorspeedSetting = EEPROM.read(mspeed_rear);     
     brakeSetting = EEPROM.read(brake_rear);
     hangtimeSetting = EEPROM.read(hang_rear) * 100; 
-    compSetting = EEPROM.read(comp_rear); 
+    idleSetting = EEPROM.read(idle_rear); 
     modeSetting = EEPROM.read(mode_rear); 
     burstSetting = EEPROM.read(burst_rear); 
     profileSwitch = "Rear"; 
@@ -95,7 +95,7 @@ void savevalues(int savePosition) {
     EEPROM.write(mspeed_for, motorspeedSetting); 
     EEPROM.write(brake_for, brakeSetting); 
     EEPROM.write(hang_for, (hangtimeSetting / 100));
-    EEPROM.write(comp_for, compSetting); 
+    EEPROM.write(idle_for, idleSetting); 
     EEPROM.write(mode_for, modeSetting); 
     EEPROM.write(burst_for, burstSetting); 
     break;
@@ -104,7 +104,7 @@ void savevalues(int savePosition) {
     EEPROM.write(mspeed_mid, motorspeedSetting); 
     EEPROM.write(brake_mid, brakeSetting); 
     EEPROM.write(hang_mid, (hangtimeSetting / 100));
-    EEPROM.write(comp_mid, compSetting); 
+    EEPROM.write(idle_mid, idleSetting); 
     EEPROM.write(mode_mid, modeSetting);
     EEPROM.write(burst_mid, burstSetting);
     break;
@@ -113,7 +113,7 @@ void savevalues(int savePosition) {
     EEPROM.write(mspeed_rear, motorspeedSetting); 
     EEPROM.write(brake_rear, brakeSetting); 
     EEPROM.write(hang_rear, (hangtimeSetting / 100));
-    EEPROM.write(comp_rear, compSetting); 
+    EEPROM.write(idle_rear, idleSetting); 
     EEPROM.write(mode_rear, modeSetting);
     EEPROM.write(burst_rear, burstSetting);
     break;
